a1f5beb10c68637e549a4085f2af1d455d6df5ea
Mert Bozkurt <mbozkurt@deloitte.com>
2021-08-21 16:38:21 +0300

    tempSensor variable names are updated.

--- opcua_service.py
+++ opcua_service.py
@@ -5,13 +5,13 @@ from opcua import Server
 
 server = Server()
 server.set_endpoint("opc.tcp://127.0.0.1:12345")
-server.register_namespace("Production Line 1")
+server.register_namespace("productionLine1")
 
 objects = server.get_objects_node()
-tempSensor = objects.add_object('ns=2;s="TS"', "Temperature Sensor")
-tempSensor.add_variable('ns=2; s="TS_VendorName"', "TS Vendor Name", "Deloitte")
-tempSensor.add_variable('ns=2; s="TS_SerialNumber"', "TS Serial Number", 1984)
-temp = tempSensor.add_variable('ns=2; s="TS_Temperature"', "TS Temperature", 20)
+tempSensor = objects.add_object('ns=2;s="tempSensor"', "tempSensor")
+tempSensor.add_variable('ns=2; s="vendorName"', "vendorName", "Deloitte")
+tempSensor.add_variable('ns=2; s="serialNumber"', "serialNumber", 1984)
+temp = tempSensor.add_variable('ns=2; s="temperature"', "temperature", 20)
 
 temperature = 20.0
 try:
@@ -19,9 +19,9 @@ try:
     server.start()
     print("Service is running")
     while True:
-        temperature += random.uniform(-1,1)
+        temperature += random.uniform(-1, 1)
         temp.set_value(temperature)
-        print("New Temperature: "+ str(temp.get_value()))
+        print("New Temperature: " + str(temp.get_value()))
         sleep(2)
 finally:
     server.stop()
